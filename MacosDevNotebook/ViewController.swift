a00953966d79209ccb248f480e0efa7dd3946eb6
Huawei <24174126@qq.com>
2022-11-08 01:53:34 +0800

    Initial Commit

---
+++ MacosDevNotebook/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MacosDevNotebook
+//
+//  Created by mabs on 08/11/2022.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
